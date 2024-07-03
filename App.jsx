950bed717a159c2476038cdc4e35d69c5c04fc57
Satheeska Arulanantham <sindhu.arul1695@gmail.com>
2024-07-03 10:52:54 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import CurrencyConvertor from './Components/CurrencyConvertor'
+import './App.css'
+
+function App() {
+  return <div className='min-h-screen bg-gray-100 flex flex-col items-center justify-center'>
+    <div className='container'>
+      <CurrencyConvertor />
+    </div>
+  </div>
+}
+
+export default App
